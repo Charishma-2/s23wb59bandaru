5c9bfba8d8c2311946fd7e9c8a53b8495a0bb1b9
Charishma-2 <charishmachowdarybandaru@gmail.com>
2023-04-04 13:03:10 -0500

    edited file

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', tennisRouter);
+app.use('/tennis', tennisRouter);
 
 
 // catch 404 and forward to error handler
